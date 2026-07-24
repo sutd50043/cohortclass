29f6039a908d5faa212d15ff3f87d609f97e6490
Qin Yanxia <qolina@gmail.com>
2026-07-24 09:19:15 +0800

    add ex1.py

---
+++ cc10/ex1.py
@@ -0,0 +1,13 @@
+from functools import reduce
+# use python's reduce function to finish exercise 1
+
+def min(lst):
+    return reduce(???)
+
+def max(lst):
+    return reduce(???)
+
+
+assert 7 == min([32, 63, 7, 10, 100])
+
+assert 100 == max([32, 63, 7, 10, 100])
